af24179f3171c34736df509fd781de3b96d3f0a6
Talita <talitazmiza@gmail.com>
2022-06-16 15:17:17 +0700

    new footer

---
+++ src/Views/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return(
+        <footer className="py-6 text-gray-600 text-center text-sm bg-black text-red-400">
+          <span className="text-grey-900 font-bold text-lg mr-2">
+            Lita
+          </span>&copy; {new Date().getFullYear()} All Rights
+        </footer>
+    );
+}

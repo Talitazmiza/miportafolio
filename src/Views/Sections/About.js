7f9c9f81ecf9e4067d36130064e299999f895c4f
Talita <talitazmiza@gmail.com>
2022-06-16 14:52:45 +0700

    edit about

--- src/Views/Sections/About.js
+++ src/Views/Sections/About.js
@@ -4,8 +4,11 @@ import Typewriter from "typewriter-effect";
 
 export default function About() {
     return (
-        <div className="mx-auto bg-white overflow-none">
-            <div className="grid grid-cols-1 md:grid-cols-2 divide-x place-items-center">
+        <div className="bg-white overflow-none">
+            <div className="animate-[wiggle_1s_ease-in-out_infinite]">
+                <h1 className="text-2xl">🔥 under construction 🔥</h1>
+            </div>
+            <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 divide-x place-items-center gap-3">
                 <div className="md:flex lg:overflow-y-visible">
                     <div className="lg:shrink-0">
                         <img src={intro} alt="me" className="object-cover h-96 w-96 md:h-96 md:w-96" />
@@ -54,10 +57,10 @@ export default function About() {
                             <br />
                             <div className="grid grid-cols-4 gap-4 place-items-center">
                                 <img className="object-scale-down h-20 w-20" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png" alt="JavaScript"/>
-                                <img className="object-scale-down h-20 w-20" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="React"/>
+                                <img className="object-scale-down h-20 w-20" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="React.js"/>
                                 <img className="object-scale-down h-20 w-20" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1969px-Laravel.svg.png" alt="Laravel"/>
                                 <img className="object-scale-down h-20 w-20" src="https://miro.medium.com/max/800/0*cQTv5n6xV7opBBIB" alt="ExpressJS"/>
-                                <img className="object-scale-down h-20 w-20" src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="Git"/>
+                                <img className="object-scale-down h-20 w-20" src="https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png" alt="Next.js"/>
                                 <img className="object-scale-down h-20 w-20" src="https://brandlogos.net/wp-content/uploads/2016/06/bootstrap-logo-vector-download.jpg" alt="Bootstrap"/>
                                 <img className="object-scale-down h-20 w-20" src="https://tailwindcss.com/_next/static/media/social-square.b622e290e82093c36cca57092ffe494f.jpg" alt="Tailwind CSS"/>
                                 <img className="object-scale-down h-20 w-20" src="https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png" alt="MySQL"/>

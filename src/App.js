bd81bef109e414c06c6865726f7698fe0d572676
Talita <talitazmiza@gmail.com>
2022-06-16 15:16:58 +0700

    adding route path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
 import Home from './Views/Sections/Home';
+import Project from './Views/Sections/Project';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Home />
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/proyectos" exact component={Project} />
+                </Switch>
+            </Router>
         </div>
     );
 }

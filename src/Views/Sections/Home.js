69fa1b1d19c6fcac6e77f67ad5c789a9cc7aac70
Talita <talitazmiza@gmail.com>
2022-06-16 15:18:21 +0700

    delete unused codes

--- src/Views/Sections/Home.js
+++ src/Views/Sections/Home.js
@@ -2,17 +2,11 @@ import React from "react";
 import '../../App.css';
 import Navbar from "../Components/Navbar";
 import About from "./About";
+import Footer from "../Components/Footer";
 
 export default function Home() {
     return (
-        <div className="flex flex-col h-screen bg-white text-black">
-            <div className="animate-[wiggle_1s_ease-in-out_infinite]">
-                <h1 className="text-2xl">🔥 under construction 🔥</h1>
-            </div>
-            {/* <h1 data-text="🔥 Hover and wave 🔥" class="text-2xl relative overflow-hidden pb-8 before:content-[attr(data-text)attr(data-text)] before:underline before:underline-offset-8 before:decoration-wavy before:decoration-red-400 before:absolute before:whitespace-nowrap before:text-transparent hover:before:animate-wiggle">
-            🔥 under construction 🔥
-            </h1> */}
-            <Navbar />
+        <div className="flex flex-col text-black">
             <About />
         </div>
     )
